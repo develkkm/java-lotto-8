5db9e2ceeb813ff137492bd2bea4311c136e1ebc
develkkm <kkm8905508@daum.net>
2025-10-30 19:42:39 +0900

    feat(Payment): 입력 받은 금액에 대한 클래스 생성
    - 입력 받은 금액을 가지는 Payment 클래스
    - 금액이 로또 가격 미만이거나 로또 가격으로 나누어지는지 검증
    - 금액으로 로또 개수 계산

---
+++ src/main/java/lotto/model/Payment.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+public class Payment {
+    public static final int LOTTO_PRICE = 1000;
+
+    private final int money;
+
+    private Payment(int money){
+        validateAmount(money);
+        this.money = money;
+    }
+
+    public static Payment from(int money){
+        return new Payment(money);
+    }
+
+    private void validateAmount(int money){
+        validateMoneyRange(money);
+        validateDivideByLottoPrice(money);
+    }
+
+    private void validateMoneyRange(int money) {
+        if(money < LOTTO_PRICE) {
+            throw new IllegalArgumentException("금액은 " + LOTTO_PRICE + "원 이상이여야 합니다.");
+        }
+    }
+
+    private void validateDivideByLottoPrice(int money) {
+        if(money % LOTTO_PRICE != 0){
+            throw new IllegalArgumentException("금액은" + LOTTO_PRICE + "원 단위로 나누어져야합니다.");
+        }
+    }
+
+    public int getLottoAmount(){
+        return money / LOTTO_PRICE;
+    }
+}
