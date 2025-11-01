bd6773427bfb75a0026feb3167538f9ed7520f32
develkkm <kkm8905508@daum.net>
2025-11-01 09:31:13 +0900

    feat(LottoShop): 로또 발행 기능 추가
    - 숫자 발행기를 받아서 로또 shop 생성
    - 금액을 받아 `sell` 호출 시 로또 발행

---
+++ src/main/java/lotto/model/LottoShop.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import lotto.model.generator.LottoNumberGenerator;
+
+public class LottoShop {
+    private final LottoNumberGenerator generator;
+
+    private LottoShop(LottoNumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public static LottoShop of(LottoNumberGenerator generator) {
+        return new LottoShop(generator);
+    }
+
+    public List<Lotto> sell(Payment payment) {
+        int lottoAmount = payment.getLottoAmount();
+        return generateLottos(lottoAmount);
+    }
+
+    private List<Lotto> generateLottos(int amount) {
+        return IntStream.range(0, amount)
+                .mapToObj(i -> Lotto.of(generator.generate(
+                        Lotto.LOTTO_MIN_NUMBER,
+                        Lotto.LOTTO_MAX_NUMBER,
+                        Lotto.LOTTO_NUMBER_SIZE
+                )))
+                .toList();
+    }
+}
