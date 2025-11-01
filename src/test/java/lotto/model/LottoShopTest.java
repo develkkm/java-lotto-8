b898325b3077ee95d3fbf568c8abcdb77132348d
develkkm <kkm8905508@daum.net>
2025-11-01 09:29:46 +0900

    test(LottoShopTest): 로또 발행 기능 테스트
    - 금액에 비례해서 발행된 로또 개수와 번호 테스트
    - 고정 발행기(1,2,3,4,5,6) 구현

---
+++ src/test/java/lotto/model/LottoShopTest.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.generator.LottoNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoShopTest {
+
+    static class FixedGenerator extends LottoNumberGenerator {
+        @Override
+        public List<Integer> generate(int min, int max, int size) {
+            List<Integer> numbers = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                numbers.add(min + i);
+            }
+            return numbers;
+        }
+    }
+
+    @Test
+    @DisplayName("Payment 금액에 비례해 지정된 개수만큼 로또를 발행한다")
+    void shouldSellLottosByPaymentAmount() {
+        // given
+        LottoShop shop = LottoShop.of(new FixedGenerator());
+        Payment payment = Payment.from(5000);
+
+        // when
+        List<Lotto> lottos = shop.sell(payment);
+
+        // then
+        assertThat(lottos).hasSize(5);
+        assertThat(lottos.getFirst().getNumbers()).containsExactly(1,2,3,4,5,6);
+    }
+}
