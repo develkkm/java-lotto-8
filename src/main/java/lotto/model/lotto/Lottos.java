ac6c56f7a78be919e220858b52df5dad0c14264a
develkkm <kkm8905508@daum.net>
2025-11-03 18:59:21 +0900

    feat(Lottos): Lotto 일급 컬렉션 추가
    - Lotto List를 불변으로 저장
    - 전체 로또 개수, 조회 가능한 기능 제공

---
+++ src/main/java/lotto/model/lotto/Lottos.java
@@ -0,0 +1,23 @@
+package lotto.model.lotto;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = List.copyOf(lottos);
+    }
+
+    public static Lottos from(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int getSize(){
+        return lottos.size();
+    }
+}
